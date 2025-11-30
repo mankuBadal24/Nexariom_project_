327cb4f4096c3de1a0d1867dfb4550d9eebdf421
Harxsh-web <hk3631311@gmail.com>
2025-11-30 23:41:38 +0530

    route change

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,10 @@
 import { Home } from './pages/Home'
 import './App.css'
-import { Hero } from './components/ui/Hero'
 const App =() =>{
   return (
     <div>
       <Home />
-      <Hero />
+     
     </div>
   )
 }
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,3 +1,4 @@
+import { Hero } from "../components/ui/Hero"
 import Navbar from "./Navbar"
 
 
@@ -6,6 +7,9 @@ export const Home = () => {
   return (
     <div>
       <Navbar/>
+      <div className="py-20">
+         <Hero />
+      </div>
     </div>
   )
 }
