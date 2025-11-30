92019a65fe4875ebde112c1340e30c551cf84b68
Harxsh-web <hk3631311@gmail.com>
2025-11-30 22:44:17 +0530

    setup

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import './App.css'
+import { Button } from './components/ui/button'
+const App =() =>{
+  return (
+    <div>
+      <Button>Click me</Button>
+    </div>
+  )
+}
+
+export default App
